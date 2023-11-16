77f057f3240d13537dd7e0ad3ae4d7f4e3f98fb1
xneyder <xneyder@gmail.com>
2023-11-15 20:52:19 -0500

    Add files

--- main.tf
+++ main.tf
@@ -98,16 +98,16 @@ resource "aws_kms_grant" "this" {
   for_each = { for k, v in var.grants : k => v if var.create }
 
   name              = each.key
-  key_id            = try(aws_kms_key.this[0].key_id, aws_kms_external_key.this[0].id, aws_kms_replica_key.this[0].key_id, aws_kms_replica_external_key.this[0].key_id)
+  key_id            = try(aws_kms_key.this[0].arn, aws_kms_external_key.this[0].arn, aws_kms_replica_key.this[0].arn, aws_kms_replica_external_key.this[0].arn)
   grantee_principal = each.value.grantee_principal
   operations        = each.value.operations
 
   dynamic "constraints" {
-    for_each = length(lookup(each.value, "constraints", {})) == 0 ? [] : [each.value.constraints]
+    for_each = length(lookup(each.value, "constraints", {})) == 0 ? [] : each.value.constraints
 
     content {
-      encryption_context_equals = try(constraints.value.encryption_context_equals, null)
-      encryption_context_subset = try(constraints.value.encryption_context_subset, null)
+      encryption_context_equals = try(constraints.value.encryption_context_equals, {})
+      encryption_context_subset = try(constraints.value.encryption_context_subset, {null})
     }
   }
 
