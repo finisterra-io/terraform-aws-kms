4666f1b010453adda2cbaf1f523bf71878f94626
Julian <eng@finisterra.io>
2024-02-06 20:56:20 -0500

    Remove unused variables and configurations

--- variables.tf
+++ variables.tf
@@ -92,96 +92,6 @@ variable "valid_to" {
   default     = null
 }
 
-variable "enable_default_policy" {
-  description = "Specifies whether to enable the default key policy. Defaults to `true`"
-  type        = bool
-  default     = true
-}
-
-variable "key_owners" {
-  description = "A list of IAM ARNs for those who will have full key permissions (`kms:*`)"
-  type        = list(string)
-  default     = []
-}
-
-variable "key_administrators" {
-  description = "A list of IAM ARNs for [key administrators](https://docs.aws.amazon.com/kms/latest/developerguide/key-policy-default.html#key-policy-default-allow-administrators)"
-  type        = list(string)
-  default     = []
-}
-
-variable "key_users" {
-  description = "A list of IAM ARNs for [key users](https://docs.aws.amazon.com/kms/latest/developerguide/key-policy-default.html#key-policy-default-allow-users)"
-  type        = list(string)
-  default     = []
-}
-
-variable "key_service_users" {
-  description = "A list of IAM ARNs for [key service users](https://docs.aws.amazon.com/kms/latest/developerguide/key-policy-default.html#key-policy-service-integration)"
-  type        = list(string)
-  default     = []
-}
-
-variable "key_service_roles_for_autoscaling" {
-  description = "A list of IAM ARNs for [AWSServiceRoleForAutoScaling roles](https://docs.aws.amazon.com/autoscaling/ec2/userguide/key-policy-requirements-EBS-encryption.html#policy-example-cmk-access)"
-  type        = list(string)
-  default     = []
-}
-
-variable "key_symmetric_encryption_users" {
-  description = "A list of IAM ARNs for [key symmetric encryption users](https://docs.aws.amazon.com/kms/latest/developerguide/key-policy-default.html#key-policy-users-crypto)"
-  type        = list(string)
-  default     = []
-}
-
-variable "key_hmac_users" {
-  description = "A list of IAM ARNs for [key HMAC users](https://docs.aws.amazon.com/kms/latest/developerguide/key-policy-default.html#key-policy-users-crypto)"
-  type        = list(string)
-  default     = []
-}
-
-variable "key_asymmetric_public_encryption_users" {
-  description = "A list of IAM ARNs for [key asymmetric public encryption users](https://docs.aws.amazon.com/kms/latest/developerguide/key-policy-default.html#key-policy-users-crypto)"
-  type        = list(string)
-  default     = []
-}
-
-variable "key_asymmetric_sign_verify_users" {
-  description = "A list of IAM ARNs for [key asymmetric sign and verify users](https://docs.aws.amazon.com/kms/latest/developerguide/key-policy-default.html#key-policy-users-crypto)"
-  type        = list(string)
-  default     = []
-}
-
-variable "key_statements" {
-  description = "A map of IAM policy [statements](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document#statement) for custom permission usage"
-  type        = any
-  default     = {}
-}
-
-variable "source_policy_documents" {
-  description = "List of IAM policy documents that are merged together into the exported document. Statements must have unique `sid`s"
-  type        = list(string)
-  default     = []
-}
-
-variable "override_policy_documents" {
-  description = "List of IAM policy documents that are merged together into the exported document. In merging, statements with non-blank `sid`s will override statements with the same `sid`"
-  type        = list(string)
-  default     = []
-}
-
-variable "enable_route53_dnssec" {
-  description = "Determines whether the KMS policy used for Route53 DNSSEC signing is enabled"
-  type        = bool
-  default     = false
-}
-
-variable "route53_dnssec_sources" {
-  description = "A list of maps containing `account_ids` and Route53 `hosted_zone_arn` that will be allowed to sign DNSSEC records"
-  type        = list(any)
-  default     = []
-}
-
 ################################################################################
 # Replica Key
 ################################################################################
@@ -228,12 +138,6 @@ variable "computed_aliases" {
   description = "A map of aliases to create. Values provided via the `name` key of the map can be computed from upstream resources"
   type        = any
   default     = {}
-}
-
-variable "aliases_use_name_prefix" {
-  description = "Determines whether the alias name is used as a prefix"
-  type        = bool
-  default     = false
 }
 
 ################################################################################
